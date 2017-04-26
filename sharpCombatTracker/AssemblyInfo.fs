30b830f160e62379ae11b0294d357160a78ab616
Alec Parks <alec.parks@gmail.com>
2017-04-25 20:42:31 -0500

    Initial commit and setup.
    Initial setup and work on Combatant.

---
+++ sharpCombatTracker/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace sharpCombatTracker
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("sharpCombatTracker")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("${AuthorCopyright}")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
