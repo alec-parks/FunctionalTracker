143900f826fa1fb069049b24f718ebda0433a4a4
Alec Parks <alec.parks@gmail.com>
2017-04-27 08:46:12 -0500

    Update roller and Combatant record
    Updated and added roller module.
    
    Expanded combatant record to have optional Initiative.
    
    Included example documentation at the end (for my benefit)

--- sharpCombatTracker/Combatant.fs
+++ sharpCombatTracker/Combatant.fs
@@ -1,11 +1,19 @@
-﻿type CombatantType = | Player = 1 | Monster = 2
+﻿module DiceRoller =
+    let roller = System.Random()
+    let rec roll sides dice = 
+        match dice with
+        | 1 -> roller.Next(1, sides + 1)
+        | _ -> roller.Next(1, sides + 1) + roll sides (dice - 1)
+    let rollInitiative () = roll 20 1
+
+type CombatantType = | Player = 1 | Monster = 2
 type Status = | Alive = 1 | Dead=2
 type Name = Name of string
 type Modifier = Modifier of int
 
 //Combatant record type
 type Combatant = {Name:Name; InitiativeMod:Modifier; HP:int; MaxHP:int; 
-                    Type:CombatantType; State:Status}
+                    Type:CombatantType; State:Status; Initiative : int option}
 
 //Active Patterns
 let (|IsAlive|IsDead|) health = if health > 0 then IsAlive else IsDead
@@ -22,10 +30,23 @@ let healDamage combatant heal =
     let {HP=currentHP; MaxHP=maxHp} = combatant
     let totalHealth = if currentHP + heal > maxHp then maxHp 
                       else currentHP + heal
-    {combatant with HP=totalHealth}    
+    {combatant with HP=totalHealth}
+
+let rollInit combatant =
+    let {InitiativeMod = Modifier(modifier)} = combatant
+    {combatant with Initiative=Some(DiceRoller.rollInitiative() + modifier)}
 
 //Sample Player
 (*
     let chauncy = {Name = Name "Chauncy"; InitiativeMod = Modifier 5; HP = 3; MaxHP = 3;
-                   Type = CombatantType.Player; State = Status.Alive}
+                   Type = CombatantType.Player; State = Status.Alive; Initiative = None}
+*)
+
+//Simple initiative interactive test
+(*
+    let initiativeTest count =
+        List.init count (fun _ -> DiceRoller.rollInitiative())
+    initiativeTest 1000 |> List.filter (fun x -> x > 20 || x < 1)
+    //Should return an empty list as nothing will be above 20 or below 1 based on random's API
+    //Tests more for implementation of rolling a d20 than random's utility
 *)
